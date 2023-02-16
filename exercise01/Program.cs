59343ae3a3a3271dc06844720838800af9526dc2
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-16 11:52:52 +0300

    добавила гит игнор

---
+++ exercise01/Program.cs
@@ -0,0 +1,21 @@
+﻿int max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+}
+else if (firstNumber < secondNumber)
+{
+    max = secondNumber;
+Console.WriteLine("max = " + max.ToString() );
+} else {
+    firstNumber = secondNumber;
+    System.Console.WriteLine("числа равны");
+}
+
