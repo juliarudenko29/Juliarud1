92562c0bf1585594312f03849e1b565bfd25b4b1
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-16 14:13:05 +0300

    added a second task

---
+++ exercise02/Program.cs
@@ -0,0 +1,32 @@
+﻿int max;
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+}
+else if (firstNumber < secondNumber)
+{
+    max = secondNumber;
+}
+
+if(secondNumber > thirdNumber)
+{
+    max = secondNumber;
+}
+else if (secondNumber < thirdNumber)
+{
+    max = thirdNumber;
+Console.WriteLine("max = " + max.ToString() );
+}
+
+
+
