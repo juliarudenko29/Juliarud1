57e70f61d109bb5ed8349faef7a1b6a9ca4028b7
Julia RudenkoE <juliarudenko29@icloud.com>
2023-02-18 10:58:24 +0300

    add task4

---
+++ exercise04/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int maxNumber = Convert.ToInt32(Console.ReadLine());
+
+
+for (int value = 1; value < maxNumber; value++)
+{
+    if (value % 2 == 0) {
+     Console.Write($"{value} ");   
+    }
+}
+
+
